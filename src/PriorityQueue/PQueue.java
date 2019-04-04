38128b519db633d93239b5eb55fea88d7f958c6f
Warner Nielsen <bwn888@gmail.com>
2019-04-03 21:16:18 -0600

    Add binarySearch method and insert method

--- src/PriorityQueue/PQueue.java
+++ src/PriorityQueue/PQueue.java
@@ -2,14 +2,13 @@ package PriorityQueue;
 
 import java.util.ArrayList;
 
-public class PQueue<T> {
-    final int MAX_QUEUE = 30;
+public class PQueue<T extends Comparable<? super T>> {
     private int size_ = 0;
     private ArrayList<T> items; // items array for pq
     
     // PQueue Constructor
     public PQueue() {
-        items = new ArrayList<>(MAX_QUEUE);
+        items = new ArrayList<>();
     }
     
     // boolean method isEmpty() to return true or false if PQueue is empty
@@ -19,20 +18,33 @@ public class PQueue<T> {
     
     // method to insert() a newItem into the pq throws exception if full
     public void insert(T item) throws PQueueException {
-        if (size_ < MAX_QUEUE) {
-            T curr = null;
-            // if the item to insert is is equal to curr then throw exception
-            if (item.equals(curr)) {
-                throw new PQueueException("PriorityQueue Exception: " +
-                        "Insertion failed, duplicate item");
-            } else {
-                // otherwise, if item doesn't equal curr add the item
-                items.add(item);
-                size_++;
-//                curr = item;
-            }
+        int curr = items.indexOf(item);
+        // if the item to insert is is equal to curr then throw exception
+        if (item.equals(items.get(curr))) {
+            throw new PQueueException("PriorityQueue Exception: " +
+                    "Insertion failed, duplicate item");
         } else {
-            throw new PQueueException("PriorityQueue Exception: Queue full");
+            // use binarySearch to find index where to add item
+            int index = binarySearch(items, 0, size_-1, item);
+            items.add(index, item);
+            size_++;
         }
+    }
+    
+    public int binarySearch(ArrayList<T> items, int first, int last, T item) {
+        int index;
+        if (first > last) {
+            index = -1;
+        } else {
+            int mid = (first + last) / 2;
+            if (item == items.get(mid)) {
+                index = mid;
+            } else if (item.compareTo(items.get(mid)) < 0) {
+                index = binarySearch(items, first, mid-1, item);
+            } else {
+                index = binarySearch(items, mid+1, last, item);
+            }
+        }
+        return index;
     }
 }
